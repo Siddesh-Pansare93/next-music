f60610b58bf67b04cf36ad8a9defc4e228b52287
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-11-08 22:00:56 +0530

    add hero section text and spotlight effect

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+
+function HeroSection() {
+  return (
+    <div className='h-auto  md:h-[40rem]  w-full flex  flex-col justify-center items-center relative mx-auto py-10'>
+        {/* h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 */}
+        <Spotlight/>
+        <div className=' p-4 w-full relative text-center '>
+            <h1 className="text-4xl font-bold ">
+            Master the art of music
+            </h1 >
+            <p className=' mt-10 mx-auto max-w-lg text-base text-normal  '>
+            Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
