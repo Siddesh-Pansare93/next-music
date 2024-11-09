e444444d72c47d9e4317149fa65b97c1f04d6373
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-11-10 03:54:47 +0530

    changed H1 style to tyewritter effect

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,23 +3,27 @@ import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import Link from 'next/link'
 import { Button } from './ui/moving-border'
+import TypeWritterEffect from './TypeWritterEffect';
 
 function HeroSection() {
   return (
-    <div className='h-auto  md:h-[40rem]  w-full flex  flex-col justify-center items-center relative mx-auto py-10'>
+    <div className='min-h-screen  md:h-[40rem]  w-full flex  flex-col justify-center items-center relative mx-auto py-10'>
       {/* h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 */}
       <Spotlight />
       <div className=' p-4 w-full relative text-center '>
-        <h1 className="text-4xl font-bold ">
-          Master the art of music
-        </h1 >
-        <p className=' mt-10 mx-auto max-w-lg text-base text-normal  '>
+        <div className="flex justify-center items-center">
+          <TypeWritterEffect/>
+        </div >
+        <p className=' mt-10 mx-auto max-w-lg text-base   '>
           Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.
         </p>
       </div>
-      <div>
-        <Link href={"/courses"}>
-        <Button borderRadius='1.75rem'>
+      <div className='mt-6'>
+        <Link href={"/courses"} >
+        <Button 
+        borderRadius='1.75rem'
+        className='bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800'
+        >
           Explore courses
         </Button>
  
