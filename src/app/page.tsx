67d56987e079dacafc890f8d678547bbf269a903
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-11-10 03:55:20 +0530

    add featured courses section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
+import FloatingCard from "../components/Floating_card"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       {/* <h1 className="text-center opacity-[0.8]" >Siddesh Music Academy </h1>  */}
       <HeroSection/>
+      <FloatingCard/>
     </main>
   )
 }
