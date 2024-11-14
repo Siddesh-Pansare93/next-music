0992dc30060dd193da41f8fc901de9333009af8b
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-11-15 02:32:56 +0530

    add sticky scroll

---
+++ src/components/WhyToChooseUs.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import { StickyScroll } from "./ui/sticky-scroll-reveal";
+import Image from "next/image";
+import schooLcontent from "../data/whyToChoose-content.json"
+
+
+const content = schooLcontent.content
+
+export function StickyScrollComponent() {
+  return (
+    <div className="mt-10">
+      <StickyScroll content={content} />
+    </div>
+  );
+}
