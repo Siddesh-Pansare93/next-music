2d35201f9eaf80e65a8e3c98f865c69563365484
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-11-15 14:52:51 +0530

    solve error

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,7 +20,7 @@ function HeroSection() {
           {isMobile && <h1 className='text-4xl font-semibold'>Master the Art of <span className="text-teal-500">Music</span></h1>}
         </div >
         <p className=' mt-10 mx-auto max-w-lg text-base   '>
-          Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.
+          Dive into our comprehensive music courses and transform your musical journey today. Whether you are a beginner or looking to refine your skills, join us to unlock your true potential.
         </p>
       </div>
       <div className='mt-6'>
