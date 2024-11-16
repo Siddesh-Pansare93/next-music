de273179a1783b16c3edb5a8e8421549efb0d16c
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-11-16 21:00:22 +0530

    completed testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import FeaturedCards from "@/components/FeaturedCards"
 import { HeroParallaxDemo } from "@/components/Parallax";
 import { useMediaQuery } from "react-responsive";
 import { StickyScrollComponent } from "@/components/WhyToChooseUs";
-import { TestimonialsCard } from "@/components/InfiniteMovingCards";
+import { TestimonialsCard } from "@/components/testimonialsCard";
 
 export default function Home() {
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
